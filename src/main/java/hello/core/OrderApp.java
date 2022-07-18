159069bf43156ff32a26eae498f4bdec4d8e63aa
junghunchoi <azzudi@naver.com>
2022-07-18 23:44:02 +0900

    18일

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -25,7 +25,7 @@ public class OrderApp {
 
         Order order = orderService.createOrder(memberID, "itemA", 10000);
 
-
+        System.out.println("order = " + order.toString());
 
 
     }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,16 +1,15 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements  OrderService{
-    private final MemoryMemberRepository memberRepository;
+    private final MemberRepository memberRepository ;
     private  DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemoryMemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
